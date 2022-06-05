100a7be2dbbf1834b943ec0b40efba16671311ca
Vera Ho <vh71886@gmail.com>
2022-06-05 14:38:58 -0400

    Divs

--- frontend/tabs.jsx
+++ frontend/tabs.jsx
@@ -7,22 +7,33 @@ class Tabs extends React.Component {
     this.state = {
       tabId: 0
     }
+
+    this.setTab = this.setTab.bind(this);
+  }
+
+  setTab(idx) {
+    this.setState({ tabId: idx }, () => {
+      console.log(this.state.tabId)
+    })
   }
 
   render() {
-    // console.log(this.props)
-    // console.log(this.props.tabArr[0]["title"])
-    const tabWidget = this.props.tabArr.map( (el, idx) => {
-      return <div key={el["title"]}>{el["content"]}</div>
+    const tabWidget = this.props.tabArr.map( (el ,idx) => {
+      return (
+        <div key={idx} className="tab-div" onClick={ () => {this.setTab(idx)}}>
+          <h1 className="tab-header">{el.title}</h1>
+        </div>)
     })
+
+    let content = this.props.tabArr[this.state.tabId].content;
 
     return (
       <div className="tabWidget">
         {tabWidget}
+        <article className="tab-content">{content}</article>
       </div>
     )
   }
 }
 
 export default Tabs;
-
